47084bb01bd64485a491f5c9df6de612c9620bbb
KrishanV <tarpeyboy07@gmail.com>
2020-05-06 17:12:07 -0700

    Finished Exam

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -14,4 +14,7 @@ export class MainNavComponent implements OnInit {
   ngOnInit() {
   }
 
+  showAbout() {
+    this.toastService.showToast('success', 5000, 'This application is designed by Krishan Verma (C) 2018')
+  }
 }
